aa3baf47a50ea7bf93e2f06a9116732f67d9e9d2
ammar falmban <devAspirant@gmail.com>
2013-11-20 00:04:41 +0900

    عمل الدالة ذاتيا

--- Recursion/main.c
+++ Recursion/main.c
@@ -8,11 +8,31 @@
 
 #include <stdio.h>
 
+void singTheSong(int numberOfSinging){
+    
+    if(numberOfSinging == 0){
+        
+        printf("There are simply no more song on the Karaoke\n");
+        
+    }else{
+        
+        printf("%d song of songs on the wall . %d song .\n",numberOfSinging,numberOfSinging);
+        int oneFewer = numberOfSinging - 1;
+        
+        printf("Take one down , pass it around , %d song of songs \n",oneFewer);
+        
+        // ----/ this funcation calls itself. /--
+        singTheSong(oneFewer);
+        
+        printf("put a song on the DVD case %d\n",numberOfSinging);
+    }
+    
+}
+
 int main(int argc, const char * argv[])
 {
 
-    // insert code here...
-    printf("Hello, World!\n");
+    singTheSong(99);
     return 0;
 }
 
